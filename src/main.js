7ce1491de10287029ed38f3f9a7025f128ee296a
Kengo Arai <you@example.com>
2018-12-14 16:06:00 +0900

    フォルダ構造を更新しました

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from "@/router";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
