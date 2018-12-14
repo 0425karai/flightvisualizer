265da63199fe46fb7ca14e885c8f37755270d465
Kengo Arai <you@example.com>
2018-12-14 17:41:55 +0900

    Vuetify を導入しました

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,20 +1,32 @@
 <template>
-  <v-content>
-    <v-container>
-      <v-layout row>
-        <v-flex xs12>
-          <h1>PBC #4 TopPage</h1>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+ <v-content>
+   <v-container>
+     <v-radio-group v-model="row" row>
+        <v-radio label="片道" value="radio-1"></v-radio>
+        <v-radio label="往復" value="radio-2"></v-radio>
+     </v-radio-group>
+   </v-container>
+   <v-container>
+     <v-layout raw wrap>
+        <v-text-field label="出発地" outline></v-text-field>
+        <v-text-field label="目的地" outline></v-text-field>
+     </v-layout>
+   </v-container>
+ </v-content>
 </template>
 
 <script>
 export default {
-  name: "Home",
+ name: "Home",
+ data () {
+     return {
+       column: null,
+       row: null
+     }
+ }
 };
 </script>
 
 <style scoped lang="scss">
 </style>
+
