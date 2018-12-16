92d410ae98da15b080dbcfc647d40e2094ccf636
Your Name <you@example.com>
2018-12-16 10:05:55 +0900

    photo整列させました

--- src/views/Photo/index.vue
+++ src/views/Photo/index.vue
@@ -1,7 +1,7 @@
 <template>
  <v-container grid-list-md text-xs-center>
     <v-layout v-for="(time, index) in times" :key="index" :class="getColorClass(index)" row wrap>
-      <v-flex d-flex xs12 sm12 md3>
+      <v-flex d-flex xs12 sm3 md1>
         <v-card>
           <v-card-text class="px-0">{{time.name}}
             <span style="font-size: 2em; color:#3E2723;">
@@ -10,16 +10,19 @@
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex d-flex xs12 sm3 md12>
+      <v-spacer></v-spacer>
+      <v-flex d-flex xs12 sm3 md5>
         <v-card dark color="primary">
           <v-card-text class="px-0"><v-img :src="time.recommend1" aspect-ratio="2"></v-img></v-card-text>
         </v-card>
       </v-flex>
-      <v-flex d-flex xs12 sm3 md12>
+      <v-spacer></v-spacer>
+      <v-flex d-flex xs12 sm3 md5>
         <v-card dark color="primary">
           <v-card-text class="px-0"><v-img :src="time.recommend2" aspect-ratio="2"></v-img></v-card-text>
         </v-card>
       </v-flex>
+      <v-spacer></v-spacer>
     </v-layout>
   </v-container>
 </template>
